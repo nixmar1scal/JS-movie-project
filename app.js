625605e1d2f75d947741c827d38187bc685bdf25
Nicolas <nicolas8mk@gmail.com>
2024-12-03 15:08:16 -0600

    js part

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-// "https://www.omdbapi.com/?apikey=e1e9d522&s=avengers"
-
 async function main() {
-  const response = await fetch("https://www.omdbapi.com/?apikey=e1e9d522&s=avengers");
-  const moviesData = await response.json();
-  console.log(moviesData);
-  const movies = moviesData.Search;
-  movies.map((movie) => {
-    const movieElement = `
-      <div class="movie">
-        <figure class="movie__img--wrapper">
-          <img 
-            class="movie__img" 
-            src="${movie.Poster}" 
-            alt="${movie.Title}" 
-          />
-        </figure>
-        <div class="movie__info">
-          <p><b>Title:</b> ${movie.Title}</p>
-          <p><b>Release date:</b> ${movie.Year}</p>
+    const response = await fetch("https://www.omdbapi.com/?apikey=e1e9d522&s=avengers");
+    const moviesData = await response.json();
+    const movies = moviesData.Search;
+    const movieListEl = document.querySelector(".movies");
+    movieListEl.innerHTML = movies
+      .map(
+        (movie) => `
+        <div class="movie">
+          <figure class="movie_img--wrapper">
+            <img 
+              class="movie_img" 
+              src="${movie.Poster}" 
+              alt="${movie.Title}" 
+            />
+          </figure>
+          <div class="movie_info">
+            <p><b>Title:</b> ${movie.Title}</p>
+            <p><b>Release date:</b> ${movie.Year}</p>
+          </div>
         </div>
-      </div>`;
-    
-    document.body.innerHTML += movieElement;
-  });
-}
-
-main();
+      `
+      )
+      .join("");
+  }
+  
+  main();
